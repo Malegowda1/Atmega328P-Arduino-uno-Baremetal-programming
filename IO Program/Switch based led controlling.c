394e8299aec31ee998db697bd41b6bef4f4f4421
Malegowda <131359648+Malegowda1@users.noreply.github.com>
2025-10-31 09:22:12 +0530

    Update Switch based led controlling.c

--- IO Program/Switch based led controlling.c
+++ IO Program/Switch based led controlling.c
@@ -1,26 +1,24 @@
-
-// LED is connected to Digital Pin 5 of the Arduino Uno, and the switch is connected to Digital Pin 12 of the Arduino Uno. Based on the switch position, the LED will
-// be turned on and off.
-#include "Ioports.h"//It is the pin mapping library which is specified in the Ports pin Mapping.h file
+//lED IS CONNECTED TO D5(ARDUINO UNO) AND SWITCH IS CONNECTED TO D12
+#include "Ioports.h"
 int main()
 {
-    // Configure PD5(D5) (LED) as output and turn off initially
-    GPIOD->Direction | = 0x20;   // Set bit 6 (D5) as output: 0b0010 0000
-    GPIOD->Bitset & = 0xDF;      // Clear bit 6 (D5): 0b1101 1111
-    
-    // Configure PB4(D12) (switch) as input
-    GPIOD->Direction &= 0xEF;   // Clear bit 5 (D12): 0b1110 1111
+    // Configure PD5 (D5) as output and turn off initially
+    GPIOD->Direction |= 0x20;   // Set 6th bit  (PD5) as output
+    GPIOD->Bitset &= ~0x20;     // Clear 6th bit  (turn off LED)
+
+    // Configure PB4 (D12) as input
+    GPIOB->Direction &= ~0x10;  // Clear 5th bit (PB4)
 
     while(1)
     {
-        // Read switch state on D12
-        if(GPIOD->Bitset & 0x10)  // Check bit 5 (D12): 0b0001 0000
+        // Read switch state on PB4
+        if(GPIOB->Bitset & 0x10)  // Check 5th bit (PB4)
         {
-            GPIOD->Bitset |= 0x20;   // Set bit 6 (D12): 0b0010 0000
+            GPIOD->Bitset |= 0x20;   // Set 6th bit (PD5) - turn on LED
         }
         else
         {
-            GPIOD->Bitset &= 0xDF;   // Clear bit 6 (D12): 0b1101 1111
+            GPIOD->Bitset &= ~0x20;  // Clear 6th bit (PD5) - turn off LED
         }
     }
 }
