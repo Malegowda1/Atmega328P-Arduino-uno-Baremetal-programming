fc724c9458c60e384f13ed9117bbe4886265e55a
Malegowda <131359648+Malegowda1@users.noreply.github.com>
2025-11-03 12:31:55 +0530

    Update and rename SPI Programming to SPI Programming.c

--- SPI/SPI Programming.c
+++ SPI/SPI Programming.c
@@ -1,19 +1,19 @@
 #include "Common.h"
 void spi_inti()//Intialisation
 {
-  SPI_->Spi_control_Register = 0x51;//0101 0001 SPI Enable,Data order(MSB or LSB First),Master or slave select,clock polarity,clock phase,SPI Clock rate select
+  SPI_->Spi_control_Register = 0x51;//0101 0001 SPI Enable,Data order(MSB or LSB First),Master or slave select,clock polarity,clock phase,SPI Clock rate select,Prescale(16)
   SPI_->Spi_Status_Register = 0x00;
   SPI_->Spi_data_regsister = 0x00;
 }
 
 void configuration()
 {
- GPIOB->Direction=0x2C;
+ GPIOB->Direction=0x2C;//00101100
 }
 
 void spi_send_dta()
 {
-  SPI_->Spi_control_Register = 0xAB;
+  SPI_->Spi_control_Register = 0xAB;//1010 1011
    while(!((SPI_->Spi_Status_Register) & (0x80)));
    SPI_->Spi_Status_Register = 0x80;
 }
