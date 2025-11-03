ac8ff8717f4e05f1702c31982f89ab354801b9a2
Malegowda <131359648+Malegowda1@users.noreply.github.com>
2025-11-03 11:59:48 +0530

    Add UART data sending functionality
    Implement UART data sending functions including initialization and baud rate setting.

---
+++ UART/Data Sending in UART.h
@@ -0,0 +1,34 @@
+//Formula = (Fosc/16*requirebaud)-1
+#include"Common.h"
+uint32_t UartDat;
+uint32_t BaudrateSet(const uint32_t baud)
+{
+
+   uint16_t temp;
+   temp = ((Systemclk)/(baudratePrescale*baud))-1;   //103 UbrrH = 0000 0000  ubbrl = 0110 0111  0000 0000 0110 0111
+   USART->UBBrAn[0] = (uint8_t)temp;
+   USART->UBBrAn[1] = (uint8_t)(temp>>8);
+   return temp;
+}
+
+
+void UartInitialize()
+{
+   USART->UArtCSB = 0x18; // Enable TX and RX (TXEN=1, RXEN=1)
+   USART->UArtCSC = 0x06;// 8-bit data, 1 stop bit, no parity
+}
+
+void Uart_Send(unsigned char UartDat)
+{
+  while (!(USART->UArtCSA & 0x20));//Wait to until the register is empty
+  USART->UDn = UartDat;// load UartDat into the the udn for transmission
+}
+
+
+int main()
+{
+ UartInitialize();
+ BaudrateSet(9600);
+ Uart_Send('A');
+
+}
